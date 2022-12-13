ac6bf5c53d1f46bcbd3f126f06831c7b5fb11940
AnasMahrous <anasmahrous01@gmail.com>
2022-12-13 11:10:59 -0800

    3rd task fixx

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,11 @@
 void print_alphabet_x10(void)
 
 {
-	for (int i = 0; i < 10; i++)
+	int i;
+	int j;
+	for (i = 0; i < 10; i++)
 	{
-		for (char j = 'a'; j <= 'z'; j++)
+		for (j = 'a'; j <= 'z'; j++)
 		{
 			_putchar(j);
 		}
