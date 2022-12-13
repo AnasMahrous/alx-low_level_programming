09ba75708c17da04d920e756c11e91b1beb6f5ff
AnasMahrous <anasmahrous01@gmail.com>
2022-12-13 11:01:36 -0800

    2nd task

---
+++ 1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+
+{
+	while ("a" <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
+
+}
