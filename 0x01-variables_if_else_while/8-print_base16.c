4b2330d56c6fb57a6902a48ad4c58663ce9f7690
AnasMahrous <anasmahrous01@gmail.com>
2022-12-27 18:44:07 -0800

    done

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,16 +3,24 @@
 
 /*
  * main - prints all the numbers of base 16 in lowercase
+ *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	char num = 57;
-	for(char i='0';i<='f';i++){
-		if(i<58||i>96){
-			putchar(i);
-		}
+	int num = '0'; /*Initialize num to 0*/
+	char letter = 'a'; /*Initialize letter to a*/
+
+	while (num <= '9') /*Print 0-9*/
+	{
+		putchar(num);
+		num++;
+	}
+	while (letter <= 'f') /*Print a-f*/
+	{
+		putchar(letter);
+		letter++;
 	}
 
 	putchar('\n');
