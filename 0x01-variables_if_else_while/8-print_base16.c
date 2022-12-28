04c38da8876624d04756162b5e683647c33eda5c
AnasMahrous <anasmahrous01@gmail.com>
2022-12-27 18:25:28 -0800

    another solution

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,18 +8,12 @@
 
 int main(void)
 {
-	int num = '0'; /*Initialize num to 0*/
-	char letter = 'a'; /*Initialize letter to a*/
+	char num = 57; /*Initialize num to 0*/
 
-	while (num <= '9') /*Print 0-9*/
-	{
-		putchar(num);
-		num++;
-	}
-	while (letter <= 'f') /*Print a-f*/
-	{
-		putchar(letter);
-		letter++;
+	for(char i='0';i<='f';i++){
+		if(i<58||i>97){
+			putchar(i);
+		}
 	}
 
 	putchar('\n');
