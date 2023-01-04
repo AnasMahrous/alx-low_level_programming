d47c38d07c14c07c988f6b94837f7cac49943024
AnasMahrous <anasmahrous01@gmail.com>
2023-01-04 14:36:34 +0000

    another solution

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _puts_recursion - prints a string, followed by a new line.
@@ -9,8 +9,7 @@ void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		return 1 + _puts_recursion(s + 1);
 	}
 	else
 		_putchar('\n');
