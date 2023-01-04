86dd89610e5e0a1dbab8203c97b7de233fe64d56
AnasMahrous <anasmahrous01@gmail.com>
2023-01-04 14:32:25 +0000

    another solution

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,17 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
- *_puts_recursion - prints a string
- *@s: pointer block of memory to fill
- *Return: void
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string
+ * Return: no return.
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
 	if (*s != '\0')
 	{
-	return 1 + _puts_recursion(s + 1);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
+	else
+		_putchar('\n');
 }
